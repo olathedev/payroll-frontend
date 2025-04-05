4d6c2c26236ddab195b09ef24d849c7a5fe2ec7d
olathedev <olapademicheal3@gmail.com>
2025-04-05 20:01:43 +0100

    update in auth screens

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -4,24 +4,24 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="w-[75%]">
+    <div className="w-[68%]">
       <header className="flex items-center flex-col gap-0.5 pb-3">
-        <div className=" flex gap-2 mb-1 items-center">
-          <Image src="/images/logo.png" alt="logo" width={38} height={38} />
-          <p className="font-semibold text-lg text-primary-700">Paytron</p>
+        <div className=" flex gap-2 mb-4 items-center">
+          <Image src="/images/logo.png" alt="logo" width={34} height={34} />
+          <p className="font-semibold text-lg text-primary-500">Paytron</p>
         </div>
-        <h1 className="text-[22px] font-semibold text-text-900">
+        <h1 className="text-[24px] font-medium text-text-900">
           Login to your account
         </h1>
 
-        <p className="text-text-300">Securely login to you payroll account</p>
+        <p className="text-text-300">Access your paytron account securely and effortlessly</p>
       </header>
       <form action="" className="w-full mt-6 flex flex-col gap-4">
         <div className="flex flex-col gap-1">
           <label className="font-medium text-sm text-text-800">Email</label>
           <input
             type="email"
-            className="w-full py-3 px-3 border border-text-100 rounded-md text-sm"
+            className="w-full py-3 px-3 border border-text-100 rounded-xl text-sm focus:border-primary-300 focus:ring-1 focus:bg-primary-50"
             placeholder="deborahk@gmail.com"
           />
         </div>
@@ -30,12 +30,12 @@ const page = () => {
           <label className="font-medium text-sm text-text-800">Password</label>
           <input
             type="password"
-            className="w-full py-3 px-3 border border-text-100 rounded-md text-sm"
+            className="w-full py-3 px-3 border border-text-100 rounded-xl text-sm focus:border-primary-300 focus:ring-1 focus:bg-primary-50"
             placeholder="********"
           />
         </div>
 
-        <button className="cursor-pointer py-3 bg-primary-600 rounded-md text-white mt-2">
+        <button className="cursor-pointer py-3 bg-primary-600 rounded-xl text-white mt-2">
           Continue
         </button>
 
