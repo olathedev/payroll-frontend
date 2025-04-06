33edb35a4ddc35caa1f7bac9d1a8e5bd7113ae2b
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-04-06 17:38:30 +0100

    add Navbar component and update Home page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
-'use client'
+"use client";
 
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";/
 
 export default function Home() {
-  redirect("/signin");
+  // redirect("/signin");
   return (
     <div className="">
-
+      <div>
+        <p>Shows your real-time credit score.</p>
+        <h1>All your money in one place</h1>
+        <p>
+          This app tracks your credit and debit card purchases and rounds them
+          up to the nearest dollar then it invests the difference for you in
+          safe exchange-traded funds.
+        </p>
+        <div>
+          <button>Get started</button>
+        </div>
+      </div>
     </div>
   );
 }
